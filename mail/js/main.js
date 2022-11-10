e76f29f4492b349ae8974387603e5c9296fa802a
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-11-10 17:45:31 +0100

    mail interfaccia

--- mail/js/main.js
+++ mail/js/main.js
@@ -2,25 +2,34 @@
  controlla che sia nella lista di chi può accedere,
  stampa un messaggio appropriato sull’esito del controllo. */
 
-//  chiedere all'utente la sua email
-let yourMail = prompt("inserisci la tua email:")
-console.log(yourMail);
+let element = document.getElementById("controll");
+element = document.addEventListener (`click`,
+   function(event) {
+    event.preventDefault()
+    
+        //  chiedere all'utente la sua email
+        let yourMail = document.getElementById("yourmail").value;
+        console.log(yourMail);
 
-// variabile ospite
-let guest = false;
+        // variabile ospite
+        let guest = false;
 
-// controllare che sia nella lista
-const emailList = ["ciro14@napoli.com", "ziomario6@napoli.com", "lorenzo24@napoli.com"];
-console.log(emailList);
+        // controllare che sia nella lista
+        const emailList = ["ciro14@napoli.com", "ziomario6@napoli.com", "lorenzo24@napoli.com"];
+        console.log(emailList);
 
-// stampa messaggio sull'esito
-for(let i = 0; i < emailList.length; i++) {
-    if(yourMail === emailList[i]) {
-        console.log("Okay sei in lista :)");
-        guest = true;
-    } 
-}
+        // stampa messaggio sull'esito
+        for(let i = 0; i < emailList.length; i++) {
+            if(yourMail === emailList[i]) {
+                document.getElementById("result").innerHTML = "Okay sei in lista :)"
+                console.log("Okay sei in lista :)");
+                guest = true;
+            } 
+        }
 
-if(guest == false){
-    console.log("Mi spiace non sei in lista :(");
-}
+        if(guest == false){
+            document.getElementById("result").innerHTML = "Mi spiace non sei in lista :("
+            console.log("Mi spiace non sei in lista :(");
+        }
+    }     
+);
