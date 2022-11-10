b7d0e13e9560c8f1769d453cd4c5ff4129bb862d
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-11-10 17:24:37 +0100

    interfaccia dadi

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -2,19 +2,35 @@
 sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
-// generare numero randomico user
-let userNum = prompt("inserisci un numero da 1 a 6.")
-console.log(userNum);
+let element = document.getElementById("go");
+element = document.addEventListener ("click",
+    function(event){
+        event.preventDefault()
+        // generare numero randomico user
+        let userNum = document.getElementById("userNum").value;
+        console.log(userNum);
+        // generare numero randomico pc
+        let pcNum = Math.floor(Math.random() * 5) + 1;
+        document.getElementById("pcNum").innerHTML = pcNum;
+        console.log(pcNum);
 
-// generare numero randomico pc
-let pcNum = Math.floor(Math.random() * 5) + 1;
-console.log(pcNum);
+        // stabilire vincitore 
+        if(userNum > pcNum) {
+            document.getElementById("result").innerHTML = " Bravo hai vinto! :)"
+            console.log("bravo hai vinto :) ");
+        }else if(userNum < pcNum) {
+            document.getElementById("result").innerHTML = " Mi spiace hai perso! :("
+            console.log("hai perso :(");
+        }else  {
+            document.getElementById("result").innerHTML = " Avete pareggiato! :|"
+            console.log("pareggiato :!"); 
+        }
+    }
+);
 
-// stabilire vincitore 
-if(userNum > pcNum) {
-    console.log("bravo hai vinto :) ");
-}else if(userNum < pcNum) {
-    console.log("hai perso :(");
-}else  {
-    console.log("pareggiato :!"); 
-}
+let reset = document.getElementById("reset");
+reset = document.addEventListener(`click`, 
+    function(){
+        document.getElementById("form").reset();
+    }
+);
